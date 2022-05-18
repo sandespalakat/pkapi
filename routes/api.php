8dab110f6cd89b0e1e7f631b65df9c902976937e
sandeseco <sandes.eco@outlook.com>
2022-05-18 14:16:16 +0530

    Commented stateful api part in kernal
    added middleware route group in api

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::post('/logout',[AuthController::class, 'logout']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -25,4 +29,4 @@ Route::get('/users',function (){
 
 Route::post('/register',[AuthController::class, 'register']);
 Route::post('/login',[AuthController::class, 'login']);
-Route::post('/logout',[AuthController::class, 'logout']);
+

a8aa3b0e1b0845f1fcb7f9751c5255e906926d0d
sandeseco <sandes.eco@outlook.com>
2022-05-23 15:30:43 +0530

    User Registration edits
    edited the registration after changinf the db columns
    minor edits is user migration

--- database/migrations/2022_05_23_091519_alter_user_table.php
+++ database/migrations/2022_05_23_091519_alter_user_table.php
@@ -16,11 +16,11 @@ class AlterUserTable extends Migration
         Schema::table('users', function (Blueprint $table) {
             //
             $table->dropColumn('name');
-            $table->string('uuid')->after('id');
+            $table->string('uuid')->unique()->after('id');
             $table->string('first_name')->after('uuid');
             $table->string('last_name')->after('first_name');
             $table->string('phone')->unique()->nullable()->after('last_name');
-            $table->tinyInteger('role_id')->default(2)->nullable()->after('password');//recheck change default to master client user role id
+            $table->tinyInteger('role_id')->default(4)->nullable()->after('password');//recheck change default to master client user role id
             $table->tinyInteger('master_id')->nullable()->after('role_id');
             $table->tinyInteger('is_active')->default(0)->nullable()->after('master_id'); // recheck
         });
